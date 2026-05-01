a4519d40c2b0cb6087c1267b90b781c46c1f6804
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-05-01 04:15:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function getAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice")
 
     if(!response.ok) {
-        throw new Error("")
+        throw new Error("Sorry, we couldn’t load new advice. Try again.")
     }
 
     const data = await response.json()
@@ -14,13 +14,35 @@ async function getAdvice() {
     return data;
 }
 
+function renderAdvice(advice) {
+    titleText.textContent = `ADVICE #${advice.id}`
+    adviceText.textContent = `${advice.advice}`
+}
+
+function renderError(error) {
+    console.error(error)
+    titleText.textContent = "ERROR";
+    adviceText.textContent = "Sorry, we couldn’t load new advice. Try again.";
+}
+
+function setLoadingState() {
+    buttonContainer.disabled = true;
+    adviceText.textContent = "Loading advice...";
+}
+
+function removeLoadingState() {
+    buttonContainer.disabled = false;
+}
+
 buttonContainer.addEventListener("click", async () => {
     try {
-        const adviceObj = await getAdvice()
+        setLoadingState();
+        const adviceObj = await getAdvice();
 
-        titleText.textContent = `ADVICE #${adviceObj.slip.id}`
-        adviceText.textContent = `${adviceObj.slip.advice}`
+        renderAdvice(adviceObj.slip)
     } catch(error) {
-        console.log(error)
+        renderError(error);
+    } finally {
+        removeLoadingState();
     }
 })
